4980b671689972a8cbdd5abc19535372e2fe2ed5
vagrant <vagrant@precise32.(none)>
2016-02-11 21:41:04 +0000

    I'm okay at this. I think.

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -37,11 +37,22 @@ function clickadick(result)
 
 }
 
+function flipadip(result)
+{
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
+	$.get('/palette', flipadip);
+
 	console.log("User clicked on color button");
 }
